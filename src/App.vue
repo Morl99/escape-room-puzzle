47331df668ac6e3be645ab7cbe0d589db3fdb61c
Carsten Hoffmann <choffmann87@gmail.com>
2023-03-25 19:06:09 +0100

    First version

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import {RouterView} from 'vue-router'
+import type {GlobalThemeOverrides} from "naive-ui";
+import {NConfigProvider, NThemeEditor} from "naive-ui";
+
+const theme: GlobalThemeOverrides = {
+  "common": {
+    "fontSize": "20px",
+    "fontSizeLarge": "26px",
+    "fontSizeHuge": "36px",
+    "primaryColor": "#44af69FF"
+  },
+  "Button": {
+    "fontSizeMedium": "20px",
+    "heightMedium": "40px"
+  },
+  "Card": {
+    "titleFontSizeMedium": "30px"
+  }
+}
+</script>
+
+<template>
+  <header>
+  </header>
+  <n-theme-editor>
+
+    <n-config-provider :theme-overrides="theme">
+      <RouterView/>
+    </n-config-provider>
+  </n-theme-editor>
+
+</template>
+
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import {NButton, NCard, NGi, NGrid, NModal} from "naive-ui";
+import card1Image from './card-1.jpg'
+import {ref} from "vue";
+
+const showModal1 = ref(false)
+
+
+</script>
+<template>
+  <n-modal v-model:show="showModal1">
+    <n-card
+        style="width: 80vw; height: 80vh;"
+        title="Modal"
+        :bordered="false"
+        size="huge"
+        role="dialog"
+        aria-modal="true"
+    >
+      <template #header-extra>
+        Oops!
+      </template>
+      Content
+      <template #footer>
+        Footer
+      </template>
+    </n-card>
+  </n-modal>
+  <n-grid x-gap="12" :cols="3">
+    <n-gi>
+      <div class="light-green">
+        <n-card title="Mit wem?">
+          In deiner Vergangenheit bist du auf eine Gruppe von Menschen getroffen. Erinnerst du dich noch, wer das war?
+          Finde das Codewort heraus!
+          <template #cover>
+            <img :src=card1Image>
+          </template>
+          <template #footer>
+            <div class="footer">
+              <n-button @click="showModal1 = true" type="primary">Klick mich!</n-button>
+              <div class="step-indicator">1</div>
+            </div>
+          </template>
+        </n-card>
+      </div>
+    </n-gi>
+    <n-gi>
+      <div class="green"/>
+    </n-gi>
+    <n-gi>
+      <div class="light-green"/>
+    </n-gi>
+    <n-gi>
+      <div class="green"/>
+    </n-gi>
+  </n-grid>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/vars.scss";
+
+.light-green {
+  height: 108px;
+  background-color: rgba(0, 128, 0, 0.12);
+}
+
+.green {
+  height: 108px;
+  background-color: rgba(0, 128, 0, 0.24);
+}
+
+.step-indicator {
+  border-radius: 50%;
+  background: $primary;
+  color: white;
+  height: 40px;
+  width: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
